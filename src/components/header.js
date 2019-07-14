397436d45c923aa64f46d297ee3b34a63fb07cbe
aneeshack4 <aneeshack4@gmail.com>
2019-07-13 19:15:54 -0700

    Using Gatsby & Fabric to make a new personal website

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,40 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { DefaultButton, Stack } from 'office-ui-fabric-react';
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#aff8db`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
+        marignLeft: '0 auto',
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ margin: 0}}>
+        <Stack horizontal gap={250}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
+        <Stack horizontal gap={10}>
+        <DefaultButton styles={{ root: {backgroundColor: '#ff9cee', border: 'none'}}}>Now</DefaultButton>
+        <DefaultButton styles={{ root: {backgroundColor: '#ff9cee', border: 'none'}}}>Travel</DefaultButton>
+        <DefaultButton styles={{ root: {backgroundColor: '#ff9cee', border: 'none'}}}>Resume</DefaultButton>
+        </Stack>
+        </Stack>
       </h1>
     </div>
   </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi there</h1>
+    <p>I'm an engineer at Microsoft. I'm currently working in open source web dev, helping build <a href="https://developer.microsoft.com/en-us/fabric#/">Fabric</a>. </p>
+    <p>Outside of coding, I travel a lot (16 countries and counting!) and love learning new languages, dancing, tennis, and piano.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
